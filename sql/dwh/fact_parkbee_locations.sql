75fdb384fa9c5c8875c30c0431881c4fe1c7634c
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-18 15:35:17 +0100

    Create fact_parkbee_locations table from staging data

---
+++ sql/dwh/fact_parkbee_locations.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE TABLE `grand-water-473707-r8.dwh.fact_parkbee_locations` AS
+SELECT
+  splc.scrape_datetime,
+  splc.location_id,
+  splc.price_cost,
+  splc.available_spaces,
+  splc.total_spaces,
+  splc.avg_rating,
+  splc.total_review
+FROM
+  `grand-water-473707-r8.staging.staging_parkbee_locations_combined` splc
+order by 1,2
