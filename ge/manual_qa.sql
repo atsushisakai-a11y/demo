cf1bcff45f7d62b8ef8488db1e7fe012d39cc8c5
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-19 11:00:36 +0100

    Create manual_qa.sql

---
+++ ge/manual_qa.sql
@@ -0,0 +1,33 @@
+
+SELECT
+'staging_parkbee_garages' as table_name,
+  DATE_TRUNC(scrape_datetime, day) AS scrape_date,
+  COUNT(*)
+FROM
+  `grand-water-473707-r8.staging.staging_parkbee_garages`
+GROUP BY
+  ALL
+
+union all
+
+SELECT
+'staging_google_parkbee_locations' as table_name,
+  DATE_TRUNC(fetched_at, day) AS scrape_date,
+  COUNT(*)
+FROM
+  `grand-water-473707-r8.staging.staging_google_parkbee_locations`
+GROUP BY
+  ALL
+
+union all
+
+SELECT
+'staging_parkbee_locations_combined' as table_name,
+  DATE_TRUNC(scrape_datetime_parkbee, day) AS scrape_date,
+  COUNT(*)
+FROM
+  `grand-water-473707-r8.staging.staging_parkbee_locations_combined`
+GROUP BY
+  ALL
+ORDER BY
+  1
