00ce42d0dfcf5fb56c371b0569cefd8d37b0d6aa
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-19 14:01:42 +0100

    Update dim_parkbee_locations.sql

--- sql/dwh/dim_parkbee_locations.sql
+++ sql/dwh/dim_parkbee_locations.sql
@@ -1,15 +1,14 @@
 CREATE OR REPLACE TABLE `grand-water-473707-r8.dwh.dim_parkbee_locations` AS
 SELECT
-distinct
-  location_id,
-  place_id,
-  country_parkbee as country,
-  city_parkbee as city,
-  name_parkbee,
-  name_google,
-  parkbee_lat,
-  parkbee_lng,
-  google_maps_url
+  DISTINCT location_id,
+  country,
+  city,
+  name,
+  latitude,
+  longitude
 FROM
-  `grand-water-473707-r8.staging.staging_parkbee_locations_combined` l
-order by 1,2,3
+  `grand-water-473707-r8.staging.staging_parkbee_garages` l
+ORDER BY
+  1,
+  2,
+  3
