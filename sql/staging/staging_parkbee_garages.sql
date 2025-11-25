41f91fb66756b3beb6b7f7f7a21cd534952c4e65
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-25 11:06:46 +0100

    Update staging_parkbee_garages.sql

--- sql/staging/staging_parkbee_garages.sql
+++ sql/staging/staging_parkbee_garages.sql
@@ -10,14 +10,11 @@ SELECT
   pricingAndAvailability.pricing.currency AS price_currency,
   pricingAndAvailability.availability.availableSpaces AS available_spaces,
   pricingAndAvailability.availability.totalSpaces AS total_spaces,
-
-  -- Convert timestamps to CET
-  DATETIME(scrape_datetime, "Europe/Amsterdam") AS scrape_datetime_cet,
-  DATETIME(parking_from,    "Europe/Amsterdam") AS parking_from_cet,
-  DATETIME(parking_to,      "Europe/Amsterdam") AS parking_to_cet,
-
+  scrape_datetime AS scrape_datetime_cet,
+  parking_from AS parking_from_cet,
+  parking_to AS parking_to_cet,
   parking_duration_hours,
   hourly_price
-
-FROM `grand-water-473707-r8.raw.raw_parkbee_garages`
+FROM
+  `grand-water-473707-r8.raw.raw_parkbee_garages`
 WHERE id IS NOT NULL;
