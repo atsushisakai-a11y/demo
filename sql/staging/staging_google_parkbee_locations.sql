390b7983435bd25384950f1924baabf38898e6d2
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-25 15:20:28 +0100

    Update staging_google_parkbee_locations.sql

--- sql/staging/staging_google_parkbee_locations.sql
+++ sql/staging/staging_google_parkbee_locations.sql
@@ -1,14 +1,15 @@
 CREATE OR REPLACE TABLE `grand-water-473707-r8.staging.staging_google_parkbee_locations` AS
 SELECT
-  rpl.place_id,
-  rpl.name,
-  rpl.address,
-  rpl.lat,
-  rpl.lng,
-  rpl.rating,
-  rpl.user_ratings_total,
-  rpl.google_maps_url,
-  rpl.fetched_at
+  rg.place_id,
+  rg.name,
+  rg.address,
+  rg.lat,
+  rg.lng,
+  ST_GEOGPOINT(rg.lng, rg.lat) AS geom,
+  rg.rating,
+  rg.user_ratings_total,
+  rg.google_maps_url,
+  rg.fetched_at
 FROM
-  `grand-water-473707-r8.raw.raw_google_parkbee_locations` rpl
+  `grand-water-473707-r8.raw.raw_google_charging_places` rg
 ;
