76819132f827797f67b2119924d1ee25d745044a
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-25 15:46:48 +0100

    Create datamart_google_places_nearest_parkbee.sql

---
+++ sql/datamart/datamart_google_places_nearest_parkbee.sql
@@ -0,0 +1,76 @@
+CREATE OR REPLACE TABLE
+  `grand-water-473707-r8.datamart.datamart_google_places_nearest_parkbee` AS
+WITH
+  dim_fact_google AS (
+  SELECT
+    dgp.name,
+    dgp.address,
+    dgp.geom,
+    dgp.lat,
+    dgp.lng,
+    fpcl.place_id,
+    fpcl.rating,
+    fpcl.user_ratings_total,
+    fpcl.demand_score,
+    fpcl.demand_category
+  FROM
+    `grand-water-473707-r8.dwh.fact_parking_candidate_locations` fpcl
+  INNER JOIN
+    `grand-water-473707-r8.dwh.dim_google_places` dgp
+  ON
+    dgp.place_id = fpcl.place_id ),
+  join_parkbee AS (
+  SELECT
+    dfg.place_id,
+    dfg.name AS google_place_name,
+    dfg.address AS google_place_address,
+    dfg.lat AS google_lat,
+    dfg.lng AS google_lng,
+    dfg.geom AS google_geom,
+    dfg.rating AS google_rating,
+    dfg.user_ratings_total AS google_reviews,
+    dpl.location_id AS parkbee_location_id,
+    dpl.name AS parkbee_name,
+    dpl.city AS parkbee_city,
+    dpl.country AS parkbee_country,
+    dpl.latitude AS parkbee_lat,
+    dpl.longitude AS parkbee_lng,
+    dpl.geom AS parkbee_geom,
+    -- ⭐ Distance in meters
+    ST_DISTANCE(dfg.geom, dpl.geom) AS distance_meters,
+    -- Row numbering to take NEAREST ParkBee only
+    ROW_NUMBER() OVER (PARTITION BY dfg.place_id ORDER BY ST_DISTANCE(dfg.geom, dpl.geom) ) AS rn
+  FROM
+    dim_fact_google dfg
+  CROSS JOIN
+    `grand-water-473707-r8.dwh.dim_parkbee_locations` dpl )
+SELECT
+  place_id,
+  google_place_name,
+  google_place_address,
+  google_lat,
+  google_lng,
+  google_rating,
+  google_reviews,
+  parkbee_location_id,
+  parkbee_name,
+  parkbee_city,
+  parkbee_country,
+  parkbee_lat,
+  parkbee_lng,
+  distance_meters,
+  distance_meters / 1000 AS distance_km,
+  CASE
+    WHEN distance_meters <= 100 THEN '0–100m (Very Close)'
+    WHEN distance_meters <= 300 THEN '100–300m (Close)'
+    WHEN distance_meters <= 500 THEN '300–500m (Walkable)'
+    WHEN distance_meters <= 1000 THEN '0.5–1 km (Slightly Far)'
+    ELSE '> 1 km (Far)'
+END
+  AS distance_category
+FROM
+  join_parkbee
+WHERE
+  rn = 1
+ORDER BY
+  distance_meters ASC;
