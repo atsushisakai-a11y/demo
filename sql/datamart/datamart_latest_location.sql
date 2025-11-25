65fb4ca26e51bae86e6ceca6787bda0747723707
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-25 11:38:20 +0100

    Update datamart_latest_location.sql

--- sql/datamart/datamart_latest_location.sql
+++ sql/datamart/datamart_latest_location.sql
@@ -1,14 +1,17 @@
+CREATE OR REPLACE TABLE `grand-water-473707-r8.dwh.datamart_price_comparison` AS
 WITH
   latest AS (
   SELECT
     fpl.location_id,
-    MAX(fpl.scrape_datetime) AS max_scrape_datetime
+    MAX(fpl.scrape_datetime_cet) AS max_scrape_datetime_cet
   FROM
     `grand-water-473707-r8.dwh.fact_parkbee_locations` fpl
   GROUP BY
     ALL )
 SELECT
-  CAST(DATE_TRUNC(fpl.scrape_datetime, day) AS date) scrape_date,
+  CAST(DATE_TRUNC(fpl.scrape_datetime_cet, day) AS date) scrape_date,
+  fpl.parking_from_cet,
+  fpl.parking_to_cet,
   dpl.name,
   dpl.location_id,
   dpl.city,
@@ -17,20 +20,35 @@ SELECT
   dpl.longitude,
   fpl.occupancy_rate,
   fpl.price_cost,
-  fpl.occupancy,
+  fpl.hourly_price,
   fpl.available_spaces,
-  fpl.total_spaces
+  fpl.total_spaces,
+  z.zone_id,
+  z.hourly_rate AS public_hourly_price,
+  (fpl.hourly_price - z.hourly_rate) AS price_gap,
+  CASE
+    WHEN fpl.hourly_price > z.hourly_rate THEN 'Public Cheaper'
+    WHEN fpl.hourly_price < z.hourly_rate THEN 'ParkBee Cheaper'
+    ELSE 'Same'
+END
+  AS price_position
 FROM
   `grand-water-473707-r8.dwh.fact_parkbee_locations` fpl
 INNER JOIN
   latest l
 ON
   l.location_id = fpl.location_id
-  AND l.max_scrape_datetime = fpl.scrape_datetime
+  AND l.max_scrape_datetime_cet = fpl.scrape_datetime_cet
 INNER JOIN
   `grand-water-473707-r8.dwh.dim_parkbee_locations` dpl
 ON
   dpl.location_id = fpl.location_id
+LEFT JOIN
+  `grand-water-473707-r8.dwh.fact_parking_fee_amsterdam` z
+ON
+  ST_WITHIN(fpl.geom, z.geom)
+WHERE
+  dpl.country = 'NL'
 ORDER BY
   1,
   2
