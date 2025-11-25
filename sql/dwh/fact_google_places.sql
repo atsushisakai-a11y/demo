52543d91f420adeb6957352910e28dc240ab3cdc
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-25 15:28:36 +0100

    Update fact_google_places.sql

--- sql/dwh/fact_google_places.sql
+++ sql/dwh/fact_google_places.sql
@@ -1,15 +1,14 @@
 CREATE OR REPLACE TABLE `grand-water-473707-r8.dwh.fact_google_places` AS
 SELECT
-  rg.place_id,
-  rg.name,
-  rg.address,
-  rg.lat,
-  rg.lng,
-  ST_GEOGPOINT(rg.lng, rg.lat) AS geom,
-  rg.rating,
-  rg.user_ratings_total,
-  rg.google_maps_url,
-  rg.fetched_at
+  sg.place_id,
+  sg.name,
+  sg.address,
+  sg.lat,
+  sg.lng,
+  ST_GEOGPOINT(sg.lng, sg.lat) AS geom,
+  min(sg.fetched_at) as first_seen_date,
+  max(sg.fetched_at) as last_seen_date
 FROM
-  `grand-water-473707-r8.raw.raw_google_charging_places` rg
-;
+  `grand-water-473707-r8.staging.staging_google_parkbee_locations` sg
+group by all
+order by 1,2
