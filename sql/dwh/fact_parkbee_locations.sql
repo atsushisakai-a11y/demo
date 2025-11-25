bae04a63e85259641315557f15a0e480c93ab665
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-25 11:20:17 +0100

    Update fact_parkbee_locations.sql

--- sql/dwh/fact_parkbee_locations.sql
+++ sql/dwh/fact_parkbee_locations.sql
@@ -1,13 +1,20 @@
 CREATE OR REPLACE TABLE `grand-water-473707-r8.dwh.fact_parkbee_locations` AS
 SELECT
-  spg.scrape_datetime,
   spg.location_id,
+  ST_GEOGPOINT(spg.longitude, spg.latitude) AS geom,
+  spg.scrape_datetime_cet,
+  spg.parking_from_cet,
+  spg.parking_to_cet,
   spg.price_cost,
+  spg.parking_duration_hours,
+  SAFE_DIVIDE(spg.price_cost, spg.parking_duration_hours) AS hourly_price,
   spg.available_spaces,
   spg.total_spaces,
-  spg.total_spaces - spg.available_spaces as occupancy,
-  case when spg.total_spaces > 0 then (spg.total_spaces - spg.available_spaces) / spg.total_spaces else null end as occupancy_rate
+  CASE
+    WHEN spg.total_spaces > 0 THEN (spg.total_spaces - spg.available_spaces) / spg.total_spaces
+    ELSE NULL
+END
+  AS occupancy_rate
 FROM
   `grand-water-473707-r8.staging.staging_parkbee_garages` spg
-  where spg.total_spaces >= spg.available_spaces --get rid of error of available spaces > total spaces
-order by 1,2,3
+  ;
