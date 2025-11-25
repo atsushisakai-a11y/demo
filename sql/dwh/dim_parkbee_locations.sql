2673ac60371e5fc2d2c8933f272b1a0773264f74
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-25 12:06:03 +0100

    Update dim_parkbee_locations.sql

--- sql/dwh/dim_parkbee_locations.sql
+++ sql/dwh/dim_parkbee_locations.sql
@@ -5,10 +5,20 @@ SELECT
   spg.city,
   spg.name,
   spg.latitude,
-  spg.longitude,
-  ST_GEOGPOINT(spg.longitude, spg.latitude) AS geom
+  spg.longitude
 FROM
   `grand-water-473707-r8.staging.staging_parkbee_garages` spg
+  )
+SELECT
+  d.location_id,
+  d.country,
+  d.city,
+  d.name,
+  d.latitude,
+  d.longitude,
+  ST_GEOGPOINT(d.longitude, d.latitude) AS geom
+FROM
+  distincts d
 ORDER BY
   1,
   2,
