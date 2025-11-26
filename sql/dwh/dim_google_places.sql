391356aae6cbac7b57b70b70fac447ed6a3aeb5b
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-26 10:22:38 +0100

    Update dim_google_places.sql

--- sql/dwh/dim_google_places.sql
+++ sql/dwh/dim_google_places.sql
@@ -5,6 +5,7 @@ SELECT
   sg.address,
   sg.lat,
   sg.lng,
+  sg.google_maps_url,
   ST_GEOGPOINT(sg.lng, sg.lat) AS geom,
   min(sg.fetched_at) as first_seen_date,
   max(sg.fetched_at) as last_seen_date
