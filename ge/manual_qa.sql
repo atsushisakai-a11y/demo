83a138fc4e2d3947431ebfa2ce1bedd59041fae9
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-27 14:00:38 +0100

    Update manual_qa.sql

--- ge/manual_qa.sql
+++ ge/manual_qa.sql
@@ -22,6 +22,9 @@ select * from `grand-water-473707-r8.dwh.dim_parkbee_locations` order by 1;
 select * from `grand-water-473707-r8.dwh.fact_parkbee_locations` limit 10;
 select * from `grand-water-473707-r8.dwh.dim_google_places`;
 
+--DATAMART
+select * from `grand-water-473707-r8.datamart.datamart_google_places_nearest_parkbee`
+
 ----------------------------------------------------------------------------
 --Count check
 ----------------------------------------------------------------------------
