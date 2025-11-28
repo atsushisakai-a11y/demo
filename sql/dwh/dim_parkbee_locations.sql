a70531292f3f80aaa2c928ff01a722b45b534166
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-11-28 11:05:57 +0100

    Update dim_parkbee_locations.sql

--- sql/dwh/dim_parkbee_locations.sql
+++ sql/dwh/dim_parkbee_locations.sql
@@ -4,7 +4,8 @@ WITH
   latest AS (
   SELECT
     spg.location_id,
-    MAX(spg.scrape_datetime_cet) AS max_scrape_datetime_cet
+    MAX(spg.scrape_datetime_cet) AS last_seen_datetime,
+    MIN(spg.scrape_datetime_cet) AS first_seen_datetime
   FROM
     `grand-water-473707-r8.staging.staging_parkbee_garages` spg
   GROUP BY
@@ -16,14 +17,16 @@ SELECT
   spg.name,
   spg.latitude,
   spg.longitude,
-  ST_GEOGPOINT(spg.longitude, spg.latitude) AS geom
+  ST_GEOGPOINT(spg.longitude, spg.latitude) AS geom,
+  l.first_seen_datetime,
+  l.last_seen_datetime
 FROM
   `grand-water-473707-r8.staging.staging_parkbee_garages` spg
 INNER JOIN
   latest l
 ON
   l.location_id = spg.location_id
-  AND l.max_scrape_datetime_cet = spg.scrape_datetime_cet
+  AND l.last_seen_datetime = spg.scrape_datetime_cet
 ORDER BY
   1,
   2,
