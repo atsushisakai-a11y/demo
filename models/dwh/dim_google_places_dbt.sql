93d08f9f8f6e153167fc5185ba47f5ff717a67aa
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-12-02 16:26:50 +0100

    Update dim_google_places_dbt.sql

--- models/dwh/dim_google_places_dbt.sql
+++ models/dwh/dim_google_places_dbt.sql
@@ -1,15 +1,23 @@
-CREATE OR REPLACE TABLE `grand-water-473707-r8.dwh.dim_google_places_dbt` AS
+{{ config(
+    materialized = "table"
+) }}
+
 SELECT
-  sg.place_id,
-  sg.name,
-  sg.address,
-  sg.lat,
-  sg.lng,
-  sg.google_maps_url,
-  ST_GEOGPOINT(sg.lng, sg.lat) AS geom,
-  min(sg.fetched_at) as first_seen_date,
-  max(sg.fetched_at) as last_seen_date
-FROM
-  `grand-water-473707-r8.staging.staging_google_parkbee_locations_dbt` sg
-group by all
-order by 1,2
+    sg.place_id,
+    sg.name,
+    sg.address,
+    sg.lat,
+    sg.lng,
+    sg.google_maps_url,
+
+    -- geographic point
+    ST_GEOGPOINT(sg.lng, sg.lat) AS geom,
+
+    -- first and last time the place appeared in the raw feed
+    MIN(sg.fetched_at) AS first_seen_date,
+    MAX(sg.fetched_at) AS last_seen_date
+
+FROM {{ ref('staging_google_parkbee_locations_dbt') }} AS sg
+
+GROUP BY ALL
+ORDER BY 1, 2;
