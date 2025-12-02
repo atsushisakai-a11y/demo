21dce69b36f00465f0a9129fc9f9308237784287
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-12-02 16:13:11 +0100

    Create dim_google_places_dbt.sql

---
+++ models/dwh/dim_google_places_dbt.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE TABLE `grand-water-473707-r8.dwh.dim_google_places_dbt` AS
+SELECT
+  sg.place_id,
+  sg.name,
+  sg.address,
+  sg.lat,
+  sg.lng,
+  sg.google_maps_url,
+  ST_GEOGPOINT(sg.lng, sg.lat) AS geom,
+  min(sg.fetched_at) as first_seen_date,
+  max(sg.fetched_at) as last_seen_date
+FROM
+  `grand-water-473707-r8.staging.staging_google_parkbee_locations_dbt` sg
+group by all
+order by 1,2
