695ded8129578b2d2d2e9caa4d44cc43896ded3c
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-12-02 16:12:46 +0100

    Create staging_google_parking_places.sql

---
+++ models/staging/staging_google_parking_places.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE TABLE
+  `grand-water-473707-r8.staging.staging_google_parking_places` AS
+with demand_score as (
+SELECT
+  place_id,
+  name,
+  address,
+  lat,
+  lng,
+  ST_GEOGPOINT(lng, lat) AS geom,
+  types,
+  rating,
+  user_ratings_total,
+  fetched_at,
+  user_ratings_total
+    + IFNULL(rating * 10, 0) AS demand_score
+
+FROM `grand-water-473707-r8.raw.raw_google_charging_places`
+WHERE LOWER(types) LIKE '%parking%'
+  ),
+ ranked AS (
+  SELECT
+    ds.*,
+    NTILE(5) OVER (ORDER BY ds.demand_score DESC) AS demand_bucket
+  FROM demand_score ds
+)
+SELECT
+  *,
+  CASE
+    WHEN demand_bucket = 1 THEN 'High'
+    WHEN demand_bucket IN (2,3) THEN 'Medium'
+    ELSE 'Low'
+  END AS demand_category
+FROM ranked;
