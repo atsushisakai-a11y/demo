a58387f4fe6808c67d983aa4ef696526f38d9702
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-12-05 00:59:25 +0100

    Update fact_parkbee_locations_dbt.sql

--- models/dwh/fact_parkbee_locations_dbt.sql
+++ models/dwh/fact_parkbee_locations_dbt.sql
@@ -1,1 +1,39 @@
-a
+{{ config(
+    materialized = "table"
+) }}
+
+WITH spg AS (
+    SELECT * 
+    FROM {{ ref('staging_parkbee_garages_dbt') }}
+)
+
+SELECT
+    spg.location_id,
+    spg.scrape_datetime_cet,
+    spg.parking_from_cet,
+    spg.parking_to_cet,
+
+    -- Hour extracted from parking times
+    EXTRACT(HOUR FROM spg.parking_from_cet) AS parking_from_hour,
+    EXTRACT(HOUR FROM spg.parking_to_cet) AS parking_to_hour,
+
+    -- Weekday abbreviation (Mon/Tue/Wed…)
+    FORMAT_TIMESTAMP('%a', spg.parking_from_cet) AS parking_from_weekday,
+
+    spg.price_cost,
+    spg.parking_duration_hours,
+
+    -- Avoid division error
+    SAFE_DIVIDE(spg.price_cost, spg.parking_duration_hours) AS hourly_price,
+
+    spg.available_spaces,
+    spg.total_spaces,
+
+    -- Occupancy rate = (total - available) / total
+    CASE
+        WHEN spg.total_spaces > 0 THEN 
+            (spg.total_spaces - spg.available_spaces) / spg.total_spaces
+        ELSE NULL
+    END AS occupancy_rate
+
+FROM spg
