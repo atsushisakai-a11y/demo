44c7826b3464c96a3f63361164b883083ebd56f6
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-12-19 16:41:40 +0100

    Create dim_location.sql

---
+++ models/dwh/dim_location.sql
@@ -0,0 +1,51 @@
+WITH
+  latest AS (
+    SELECT
+      spg.location_id,
+      MAX(spg.scrape_datetime_cet) AS last_seen_datetime,
+      MIN(spg.scrape_datetime_cet) AS first_seen_datetime
+    FROM `grand-water-473707-r8`.`staging`.`staging_parkbee_garages` spg
+    GROUP BY spg.location_id
+  )
+SELECT
+  'parkbee' AS platform,
+  'parkbee|' || spg.location_id AS place_id,
+  spg.location_id AS external_id,
+  spg.country,
+  spg.city,
+  spg.name,
+  NULL AS address,
+  NULL AS primary_type,
+  'parking' AS location_type,
+  spg.latitude,
+  spg.longitude,
+  NULL AS url,
+  ST_GEOGPOINT(spg.longitude, spg.latitude) AS geom,
+  DATE_TRUNC(l.first_seen_datetime, DAY) AS first_seen_date,
+  DATE_TRUNC(l.last_seen_datetime, DAY) AS last_seen_date
+FROM `grand-water-473707-r8`.`staging`.`staging_parkbee_garages` spg
+INNER JOIN latest l
+  ON
+    l.location_id = spg.location_id
+    AND l.last_seen_datetime
+      = spg.scrape_datetime_cet
+UNION ALL
+SELECT
+  'google' AS platform,
+  'google|' || sg.place_id AS place_id,
+  sg.place_id AS external_id,
+  NULL AS country,
+  NULL AS city,
+  sg.name,
+  sg.address,
+  sg.primary_type,
+  sg.location_type,
+  sg.lat,
+  sg.lng,
+  sg.google_maps_url,
+  ST_GEOGPOINT(sg.lng, sg.lat) AS geom,
+  MIN(sg.fetched_at) AS first_seen_date,
+  MAX(sg.fetched_at) AS last_seen_date
+FROM `grand-water-473707-r8`.`staging`.`staging_google_parking_places` AS sg
+GROUP BY ALL
+ORDER BY 1, 2
