c2c9f7bb92f42b5c7e21944dd97fbae68a0882d8
atsushisakai-a11y <atsushi.sakai@gmail.com>
2025-12-19 16:42:42 +0100

    Update dim_location.sql

--- models/dwh/dim_location.sql
+++ models/dwh/dim_location.sql
@@ -1,10 +1,15 @@
+{{ config(
+    materialized = "table",
+    tags=["dwh"]
+) }}
+
 WITH
   latest AS (
     SELECT
       spg.location_id,
       MAX(spg.scrape_datetime_cet) AS last_seen_datetime,
       MIN(spg.scrape_datetime_cet) AS first_seen_datetime
-    FROM `grand-water-473707-r8`.`staging`.`staging_parkbee_garages` spg
+    FROM {{ ref('staging_parkbee_garages') }} spg
     GROUP BY spg.location_id
   )
 SELECT
@@ -23,7 +28,7 @@ SELECT
   ST_GEOGPOINT(spg.longitude, spg.latitude) AS geom,
   DATE_TRUNC(l.first_seen_datetime, DAY) AS first_seen_date,
   DATE_TRUNC(l.last_seen_datetime, DAY) AS last_seen_date
-FROM `grand-water-473707-r8`.`staging`.`staging_parkbee_garages` spg
+FROM {{ ref('staging_parkbee_garages') }} spg
 INNER JOIN latest l
   ON
     l.location_id = spg.location_id
@@ -46,6 +51,6 @@ SELECT
   ST_GEOGPOINT(sg.lng, sg.lat) AS geom,
   MIN(sg.fetched_at) AS first_seen_date,
   MAX(sg.fetched_at) AS last_seen_date
-FROM `grand-water-473707-r8`.`staging`.`staging_google_parking_places` AS sg
+FROM {{ ref('staging_google_parking_places') }} AS sg
 GROUP BY ALL
 ORDER BY 1, 2
